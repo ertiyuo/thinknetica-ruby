52e3853e9cb9318cd463a0c6e3ed784674e8d03f
Anastasiia Grishchenko <ertiyuo@x01d.com>
2021-09-27 16:48:08 +0500

    create fibonacci sequence up to 100

---
+++ task2/3-fibonacci_sequence.rb
@@ -0,0 +1,11 @@
+# Заполнить массив числами фибоначчи до 100
+
+fibonacci = [0, 1]
+loop do
+  x = fibonacci[-1] + fibonacci[-2]
+  break if x > 100
+
+  fibonacci << x
+end
+
+print "#{fibonacci}\n"
