7130aac512af728f9482a9e3723cafb175505e2d
Anastasiia Grishchenko <ertiyuo@x01d.com>
2021-10-12 22:52:54 +0500

    move methods to private/protected

--- railways/train.rb
+++ railways/train.rb
@@ -1,8 +1,6 @@
-# Класс Train (Поезд):
 class Train
-  attr_reader :number, :type, :speed, :carriages, :current_station
+  attr_reader :number, :type, :carriages, :current_station
 
-  # Имеет номер (произвольная строка), тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
   def initialize(number, type, carriages)
     @number = number
     @type = type
@@ -11,73 +9,74 @@ class Train
     @speed = 0
   end
 
-  # Может набирать скорость
   def speed_up(diff)
     @speed += diff
   end
 
-  # Может тормозить (сбрасывать скорость до нуля)
   def stop
     @speed = 0
   end
 
-  # Может прицеплять вагоны (по одному вагону за операцию, метод просто увеличивает количество вагонов).
-  # Прицепка вагонов может осуществляться только если поезд не движется.
   def add_carriage
-    return "Stop first" if @speed > 0
+    return "Stop first" if is_moving?
 
     @carriages += 1
   end
 
-  # Может отцеплять вагоны (по одному вагону за операцию, метод просто уменьшает количество вагонов).
-  # Отцепка вагонов может осуществляться только если поезд не движется.
   def remove_carriage
-    return "Stop first" if @speed > 0
+    return "Stop first" if is_moving?
 
     @carriages -= 1
   end
 
-  # Может принимать маршрут следования (объект класса Route).
-  # При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
   def follow_route(route)
     @route = route
     @current_station = route.stations[0]
     @current_station.arrive_train self
   end
 
-  # Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
   def go_forward
-    return "At what speed?" if @speed == 0
+    return "At what speed?" if !is_moving?
     return "It is the end!" if !next_station
 
     arrive_to self.next_station
   end
 
-  # Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад, но только на 1 станцию за раз.
   def go_back
-    return "At what speed?" if @speed == 0
+    return "At what speed?" if !is_moving?
     return "It is the beginning!" if !previous_station
 
     arrive_to self.previous_station
   end
 
-  # Может возвращать предыдущую станцию на основе маршрута
+  protected
+
+  # используется только объектом класса и потомками
+  attr_reader :route
+
+  # используется только объектом класса
   def previous_station
-    next_index = @route.stations.index(@current_station) - 1
+    next_index = route.stations.index(current_station) - 1
 
-    next_index >= 0 ? @route.stations[next_index] : nil
+    next_index >= 0 ? route.stations[next_index] : nil
   end
 
-  # Может возвращать следующую станцию на основе маршрута
+  # используется только объектом класса и потомками
   def next_station
-    next_index = @route.stations.index(@current_station) + 1
+    next_index = route.stations.index(current_station) + 1
 
-    @route.stations[next_index]
+    route.stations[next_index]
   end
 
+  # используется только объектом класса и потомками
   def arrive_to(station)
     @current_station.departure_train self
     @current_station = station
     @current_station.arrive_train self
+  end
+
+  # используется только объектом класса и потомками
+  def is_moving?
+    !@speed.zero?
   end
 end
