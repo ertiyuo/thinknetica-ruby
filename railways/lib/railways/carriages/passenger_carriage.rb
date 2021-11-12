73f1ba9bdd19d7adb30197a4c73665977b39907d
Anastasiia Grishchenko <ertiyuo@x01d.com>
2021-11-12 23:57:07 +0500

    modify passenger carriage

--- railways/lib/railways/carriages/passenger_carriage.rb
+++ railways/lib/railways/carriages/passenger_carriage.rb
@@ -1,7 +1,22 @@
 require_relative 'carriage'
 
 class PassengerCarriage < Carriage
-  def initialize
+  attr_reader :taken_seats
+
+  def initialize(seats)
+    @seats = seats
+    @taken_seats = 0
+
     super(:passenger)
+  end
+
+  def take_seat
+    raise 'No more seats!' if free_seats.zero?
+
+    @taken_seats += 1
+  end
+
+  def free_seats
+    @seats - taken_seats
   end
 end
