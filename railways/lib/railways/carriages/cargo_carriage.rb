c9384d560b01ec1d99641179c87eb53134e45984
Anastasiia Grishchenko <ertiyuo@x01d.com>
2021-11-13 00:10:27 +0500

    modify cargo carriage

--- railways/lib/railways/carriages/cargo_carriage.rb
+++ railways/lib/railways/carriages/cargo_carriage.rb
@@ -1,7 +1,22 @@
 require_relative 'carriage'
 
 class CargoCarriage < Carriage
-  def initialize
+  attr_reader :occupied_space
+
+  def initialize(space)
+    @space = space
+    @occupied_space = 0
+
     super(:cargo)
+  end
+
+  def take_space(amount)
+    raise 'No more space!' if free_space.zero?
+
+    @occupied_space += amount
+  end
+
+  def free_space
+    @space - occupied_space
   end
 end
